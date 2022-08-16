f119fb52ef0bdbbc9942091a1c137b2bd0ab3cb5
Erdem Ceylan <69710068+erdemxceylan@users.noreply.github.com>
2022-08-16 23:53:23 +0300

    Add files via upload

---
+++ src/pages/intro.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function IntroPage() {
+   return (
+      <div style={{ fontSize: '20rem' }}>
+         <p>Intro</p>
+      </div>
+   );
+}
---
+++ src/pages/result.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Result from '../components/result';
+
+export default function ResultPage() {
+   return (
+      <Result />
+   );
+}

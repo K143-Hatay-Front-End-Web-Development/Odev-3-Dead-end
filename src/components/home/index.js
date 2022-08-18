c253d31ddaac1576a2c8f74ca3dcb1d06c4d5de2
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-18 20:02:18 +0300

    home page styles are revised

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,14 +3,14 @@ import { createQuestions } from '../../assets/functions';
 import { useNavigate } from 'react-router-dom';
 import { STRINGS } from '../../assets/strings';
 import { useRound } from '../../context/use-round';
-import { useTotals } from '../../context/use-totals';
+// import { useTotals } from '../../context/use-totals';
 import * as svgs from '../../assets/svgs';
 import './styles.scss';
 import Operation from '../operation';
 
 const { ADDITION, SUBSTRACTION, MULTIPLICATION, DIVISION } = STRINGS;
 
-const { header, homeLine, footerText, footerCircle } = svgs;
+const { homeLine, footerCircle } = svgs;
 
 const operations = [
    { type: ADDITION, label: 'Toplama' },
@@ -21,7 +21,7 @@ const operations = [
 
 export default function Home() {
    const [operation, setOperation] = useState(null);
-   const { totals } = useTotals();
+   // const { totals } = useTotals();
    const { setRound } = useRound();
    const navigate = useNavigate();
    const [selectedIndex, setSelectedIndex] = useState(null);
@@ -44,33 +44,34 @@ export default function Home() {
    }
 
    return (
-      // <div className='body'>
-      <div className='container'>
-         <header>
-            <div className='header'>{header}</div>
+      <>
+         <div className='container'>
+            <div className='header'>Matematik Oyunu</div>
             <div className='line'>{homeLine}</div>
-         </header>
-         <div className='main'>
-            <section className='leftSection'>
-               <div>Puan: {totals.score}</div>
-               <div>Çözülen Sayısı: {totals.questionsSolved}</div>
-               <div>Yanlış Sayısı: {totals.wrongAnswers}</div>
-               <div>Doğru Sayısı: {totals.correctAnswers}</div>
-            </section>
-            <section className='rightSection'>
-               {operations.map((item, index) =>
-                  <Operation
-                     key={`operation-${index}`}
-                     selected={index === selectedIndex}
-                     label={item.label}
-                     onClick={() => onOperationSelect(item.type, index)}
-                  />
-               )}
-            </section>
+            <div className='main'>
+               <section>
+                  <div>Puan: 129</div>
+                  <div><p>Çözülen Sayısı</p><p>: 50</p></div>
+                  <div>Yanlış Cevap: 23</div>
+                  <div><p>Doğru Cevap</p><p>: 32</p></div>
+               </section>
+               <section>
+                  {operations.map((item, index) =>
+                     <Operation
+                        key={`operation-${index}`}
+                        selected={index === selectedIndex}
+                        label={item.label}
+                        onClick={() => onOperationSelect(item.type, index)}
+                     />
+                  )}
+               </section>
+            </div>
+            <div className='start' onClick={clickHandler}>
+               {footerCircle}
+               <p>Başla</p>
+            </div>
          </div>
-         <div className='footerCircle' onClick={clickHandler}><span>{footerText}</span>{footerCircle}
-         </div>
-      </div>
-      // </div>
+         {svgs.homePage}
+      </>
    );
 }
